542903184bd68802d82029989e3db430dfda0985
kjh0816 <readshot2@gmail.com>
2021-07-30 18:22:13 +0900

    게시물 수정 구현

--- src/main/java/com/kjh/controller/MpaUsrArticleController.java
+++ src/main/java/com/kjh/controller/MpaUsrArticleController.java
@@ -21,6 +21,7 @@ public class MpaUsrArticleController {
 	public MpaUsrArticleController(){
 		articleLastId = 0;
 		articles = new ArrayList<>();
+		makeTestArticles();
 	}
 	
 	
@@ -31,6 +32,73 @@ public class MpaUsrArticleController {
 		int id = writeArticle(title, body);
 		
 		Article article = getArticleById(id);
+		
+		return new ResultData("S-1", id + "번 게시물이 생성되었습니다.", "article", article);
+	}
+	
+	
+	@RequestMapping("/mpaUsr/article/getArticle")
+	@ResponseBody
+	public ResultData getArticle(int id) {
+		
+		Article article = getArticleById(id);
+		
+		if(article == null) {
+			return new ResultData("F-1", id + "번 게시물이 존재하지 않습니다.");
+		}
+		
+		return new ResultData("S-1", id + "번 게시물입니다.", "article", article);
+	}
+	
+	@RequestMapping("/mpaUsr/article/doDelete")
+	@ResponseBody
+	public ResultData deleteArticle(int id) {
+		
+		ResultData rd = delete(id);
+		
+		return rd;
+		
+		
+	}
+	
+	@RequestMapping("/mpaUsr/article/doModify")
+	@ResponseBody
+	public ResultData modifyArticle(int id, String title, String body) {
+		
+		ResultData rd = modify(id, title, body);
+		
+		return rd;
+	}
+	
+	public ResultData modify(int id, String title, String body) {
+		for(Article article : articles) {
+			if(article.getId() == id) {
+				article.setUpdateDate(Util.getNowDateStr());
+				article.setTitle(title);
+				article.setBody(body);
+				return new ResultData("S-1", id + "번 게시물이 수정되었습니다.", "article", article);
+			}
+		}
+		
+		return new ResultData("F-1", id + "번 게시물이 존재하지 않습니다.");
+	}
+	
+	public ResultData delete(int id) {
+		
+		for(Article article : articles) {
+			if(article.getId() == id) {
+				articles.remove(article);
+				return new ResultData("S-1", id + "번 게시물을 삭제했습니다.");
+			}
+		}
+		
+		return new ResultData("F-1", id + "번 게시물이 존재하지 않습니다.");
+	}
+	
+	private void makeTestArticles() {
+		for(int i = 0; i < 3; i++) {
+			writeArticle("제목"+i+1, "내용"+i+1);
+		}
 	}
 
 
@@ -50,7 +118,11 @@ public class MpaUsrArticleController {
 
 
 	private Article getArticleById(int id) {
-		// TODO Auto-generated method stub
+		for(Article article : articles) {
+			if(article.getId() == id) {
+				return article;
+			}
+		}
 		return null;
 	}
 
--- src/main/java/com/kjh/dto/Article.java
+++ src/main/java/com/kjh/dto/Article.java
@@ -1,5 +1,19 @@
 package com.kjh.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Article {
+	private int id;
+	private String regDate;
+	private String updateDate;
+	private String title;
+	private String body;
 
 }
