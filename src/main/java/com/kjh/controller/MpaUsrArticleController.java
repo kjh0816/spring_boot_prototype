4628c824fe69dc864284e6ac0c45fcd5b340a8df
kjh0816 <readshot2@gmail.com>
2021-07-30 18:37:28 +0900

    ArticleService 도입

--- src/main/java/com/kjh/controller/MpaUsrArticleController.java
+++ src/main/java/com/kjh/controller/MpaUsrArticleController.java
@@ -3,35 +3,37 @@ package com.kjh.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kjh.dto.Article;
 import com.kjh.dto.ResultData;
+import com.kjh.service.ArticleService;
 import com.kjh.util.Util;
 
 @Controller
 public class MpaUsrArticleController {
 	
-	private int articleLastId;
-	private List<Article> articles;
-	
-	
-	public MpaUsrArticleController(){
-		articleLastId = 0;
-		articles = new ArrayList<>();
-		makeTestArticles();
-	}
-	
+	@Autowired
+	private ArticleService articleService;
+
 	
 	@RequestMapping("/mpaUsr/article/doWrite")
 	@ResponseBody
 	public ResultData doWrite(String title, String body) {
 		
-		int id = writeArticle(title, body);
+		if(Util.isEmpty(title)) {
+			return new ResultData("F-1", "제목 입력");
+		}
+		if(Util.isEmpty(body)) {
+			return new ResultData("F-2", "내용 입력");
+		}
 		
-		Article article = getArticleById(id);
+		int id = articleService.writeArticle(title, body);
+		
+		Article article = articleService.getArticleById(id);
 		
 		return new ResultData("S-1", id + "번 게시물이 생성되었습니다.", "article", article);
 	}
@@ -40,11 +42,14 @@ public class MpaUsrArticleController {
 	@RequestMapping("/mpaUsr/article/getArticle")
 	@ResponseBody
 	public ResultData getArticle(int id) {
+		if(Util.isEmpty(id)) {
+			return new ResultData("F-1", "게시물 번호 입력");
+		}
 		
-		Article article = getArticleById(id);
+		Article article = articleService.getArticleById(id);
 		
 		if(article == null) {
-			return new ResultData("F-1", id + "번 게시물이 존재하지 않습니다.");
+			return new ResultData("F-2", id + "번 게시물이 존재하지 않습니다.");
 		}
 		
 		return new ResultData("S-1", id + "번 게시물입니다.", "article", article);
@@ -53,8 +58,11 @@ public class MpaUsrArticleController {
 	@RequestMapping("/mpaUsr/article/doDelete")
 	@ResponseBody
 	public ResultData deleteArticle(int id) {
+		if(Util.isEmpty(id)) {
+			return new ResultData("F-1", "게시물 번호 입력");
+		}
 		
-		ResultData rd = delete(id);
+		ResultData rd = articleService.delete(id);
 		
 		return rd;
 		
@@ -64,66 +72,21 @@ public class MpaUsrArticleController {
 	@RequestMapping("/mpaUsr/article/doModify")
 	@ResponseBody
 	public ResultData modifyArticle(int id, String title, String body) {
+		if(Util.isEmpty(id)) {
+			return new ResultData("F-1", "게시물 번호 입력");
+		}
+		if(Util.isEmpty(title)) {
+			return new ResultData("F-2", "제목 입력");
+		}
+		if(Util.isEmpty(body)) {
+			return new ResultData("F-3", "내용 입력");
+		}
 		
-		ResultData rd = modify(id, title, body);
+		ResultData rd = articleService.modify(id, title, body);
 		
 		return rd;
 	}
 	
-	public ResultData modify(int id, String title, String body) {
-		for(Article article : articles) {
-			if(article.getId() == id) {
-				article.setUpdateDate(Util.getNowDateStr());
-				article.setTitle(title);
-				article.setBody(body);
-				return new ResultData("S-1", id + "번 게시물이 수정되었습니다.", "article", article);
-			}
-		}
-		
-		return new ResultData("F-1", id + "번 게시물이 존재하지 않습니다.");
-	}
 	
-	public ResultData delete(int id) {
-		
-		for(Article article : articles) {
-			if(article.getId() == id) {
-				articles.remove(article);
-				return new ResultData("S-1", id + "번 게시물을 삭제했습니다.");
-			}
-		}
-		
-		return new ResultData("F-1", id + "번 게시물이 존재하지 않습니다.");
-	}
-	
-	private void makeTestArticles() {
-		for(int i = 0; i < 3; i++) {
-			writeArticle("제목"+i+1, "내용"+i+1);
-		}
-	}
-
-
-	private int writeArticle(String title, String body) {
-		articleLastId++;
-		int id = articleLastId;
-		String regDate = Util.getNowDateStr();
-		String updateDate = Util.getNowDateStr();
-		
-		Article article = new Article(id, regDate, updateDate, title, body);
-		articles.add(article);
-		
-		return id;
-		
-		
-	}
-
-
-	private Article getArticleById(int id) {
-		for(Article article : articles) {
-			if(article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
 
 }
---
+++ src/main/java/com/kjh/service/ArticleService.java
@@ -0,0 +1,84 @@
+package com.kjh.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.kjh.dto.Article;
+import com.kjh.dto.ResultData;
+import com.kjh.util.Util;
+
+@Service
+public class ArticleService {
+	
+	private int articleLastId;
+	private List<Article> articles;
+	
+	public ArticleService() {
+		articleLastId = 0;
+		articles = new ArrayList<>();
+		makeTestArticles();
+	}
+	
+	
+	public ResultData modify(int id, String title, String body) {
+		for(Article article : articles) {
+			if(article.getId() == id) {
+				article.setUpdateDate(Util.getNowDateStr());
+				article.setTitle(title);
+				article.setBody(body);
+				return new ResultData("S-1", id + "번 게시물이 수정되었습니다.", "article", article);
+			}
+		}
+		
+		return new ResultData("F-1", id + "번 게시물이 존재하지 않습니다.");
+	}
+	
+	public ResultData delete(int id) {
+		
+		for(Article article : articles) {
+			if(article.getId() == id) {
+				articles.remove(article);
+				return new ResultData("S-1", id + "번 게시물을 삭제했습니다.");
+			}
+		}
+		
+		return new ResultData("F-1", id + "번 게시물이 존재하지 않습니다.");
+	}
+	
+	private void makeTestArticles() {
+		for(int i = 0; i < 3; i++) {
+			writeArticle("제목"+i+1, "내용"+i+1);
+		}
+	}
+
+
+	public int writeArticle(String title, String body) {
+		
+		
+		articleLastId++;
+		int id = articleLastId;
+		String regDate = Util.getNowDateStr();
+		String updateDate = Util.getNowDateStr();
+		
+		Article article = new Article(id, regDate, updateDate, title, body);
+		articles.add(article);
+		
+		return id;
+		
+		
+		
+	}
+
+
+	public Article getArticleById(int id) {
+		for(Article article : articles) {
+			if(article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
+	}
+
+}
