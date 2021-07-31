7b16ef67288675b2bd61cc6c6fb5f83f1a918a16
kjh0816 <readshot2@gmail.com>
2021-07-31 15:52:10 +0900

    jsp 파일 연동. Maven dependency로 JSP 엔진, JSTL 추가. yml에 jsp 파일 인식하도록 설정. Controller, js,css 파일 생성 및 jsp 파일과 연결

---
+++ src/main/java/com/kjh/controller/MpaUsrHomeController.java
@@ -0,0 +1,16 @@
+package com.kjh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MpaUsrHomeController {
+		@RequestMapping("mpaUsr/home/main")
+		public String showMain() {
+			
+
+			return  "mpaUsr/home/main";
+		}
+	
+		
+}
